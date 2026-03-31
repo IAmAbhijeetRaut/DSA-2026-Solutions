f97a78016b391f5fdfcc6a1d6fce5df1b60dc98d
Abhijeet Raut <abhijeetsde@gmail.com>
2026-03-31 16:40:40 +0530

    feat: add solution for finding the second largest digit in a string

--- 01_Foundation/02_SecondLargest/02_SecondLargest.js
+++ 01_Foundation/02_SecondLargest/02_SecondLargest.js
@@ -1,1 +1,27 @@
 // Editorial Link: https://namastedev.com/blog/find-the-secondlargestnumber/
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var secondHighest = function(s) {
+    const digits = new Set();
+
+    // collect distinct digit characters
+    for (let i = 0; i < s.length; i++) {
+        const ch = s[i];
+        if (ch >= '0' && ch <= '9') {      // ignore letters
+            digits.add(ch);
+        }
+    }
+
+    // if fewer than 2 distinct digits, no second largest
+    if (digits.size < 2) return -1;
+
+    // convert to numbers and sort descending
+    const arr = Array.from(digits)
+        .map(d => parseInt(d, 10))
+        .sort((a, b) => b - a);
+
+    // second largest is at index 1
+    return arr[1];
+};
