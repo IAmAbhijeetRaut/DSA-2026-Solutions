fc8d71d58302de06541b47a3224f8bbc29751b9d
Abhijeet Raut <abhijeetsde@gmail.com>
2026-03-31 16:51:33 +0530

    feat: add solution for Palindrome Number problem

--- 01_Foundation/03_PalindromeNumber/03_PalindromeNumber.js
+++ 01_Foundation/03_PalindromeNumber/03_PalindromeNumber.js
@@ -1,1 +1,22 @@
 // Editorial Link: https://namastedev.com/blog/check-if-a-number-is-a-palindrome/
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  // negatives and numbers ending with 0 (but not 0 itself) are not palindromes
+  if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+  let reversedHalf = 0;
+
+  // build reversed second half of the number
+  while (x > reversedHalf) {
+    const digit = x % 10;
+    reversedHalf = reversedHalf * 10 + digit;
+    x = Math.floor(x / 10);
+  }
+
+  // for even length: x === reversedHalf
+  // for odd length: x === Math.floor(reversedHalf / 10)
+  return x === reversedHalf || x === Math.floor(reversedHalf / 10);
+};
