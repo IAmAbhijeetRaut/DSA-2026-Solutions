a056fc5954e81126e0ff97e3e4c148c28c00054c
Abhijeet Raut <abhijeetsde@gmail.com>
2026-03-31 16:06:49 +0530

    feat: add solution for sum of array elements in 01_Sum.js

--- 01_Foundation/01_Sum/01_Sum.js
+++ 01_Foundation/01_Sum/01_Sum.js
@@ -1,1 +1,12 @@
 // Editorial Link: https://namastedev.com/blog/sum_two/
+
+function giveSomeSum(args) {
+    let sum = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    return sum;
+}
+
+const sumOfAll = giveSomeSum([10, 20, 30]);
+console.log(sumOfAll);
