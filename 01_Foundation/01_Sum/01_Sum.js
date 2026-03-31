8765f677dead8bf4b144be7db98bf71af910ab1f
Abhijeet Raut <abhijeetsde@gmail.com>
2026-03-31 16:07:45 +0530

    feat: add sum implementation using reduce method

--- 01_Foundation/01_Sum/01_Sum.js
+++ 01_Foundation/01_Sum/01_Sum.js
@@ -1,12 +1,16 @@
 // Editorial Link: https://namastedev.com/blog/sum_two/
+// function giveSomeSum(args) {
+//     let sum = 0;
+//     for (let i = 0; i < args.length; i++) {
+//         sum += args[i];
+//     }
+//     return sum;
+// }
 
-function giveSomeSum(args) {
-    let sum = 0;
-    for (let i = 0; i < args.length; i++) {
-        sum += args[i];
-    }
-    return sum;
-}
+// Alternative solution using reduce
+const giveSomeSum = (args) => {
+    return args.reduce((acc, curr) => acc + curr, 0);
+};
 
 const sumOfAll = giveSomeSum([10, 20, 30]);
 console.log(sumOfAll);
